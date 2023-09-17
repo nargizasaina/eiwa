4a3b9e6d8b04a67bad02cbd653a5cdcfad7c3c0d
nurzhigitabdymanapov <abdymanapovn01@gmail.com>
2023-09-17 13:29:22 +0600

    added feature/headerPage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
+import { Content } from "./content/Content";
+import { Header } from "./header/Header";
+import { SecondContent } from "./secondContent/SecondContent";
+
 const App = () => {
-}
+  return (
+    <>
+      <Header />
+      <Content />
+      <SecondContent /> 
+    </>
+  );
+};
 
 export default App;
---
+++ src/secondContent/SecondContent.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+import image from "../assets/img/Image.svg";
+
+export const SecondContent = () => {
+  return (
+    <Container>
+      <ImageContainer>
+        <img src={image} alt="" />
+      </ImageContainer>
+      <ContentContainer>
+        <h1>Наша Миссия: Забота о Вашем Материнстве</h1>
+        <p>
+          Мы - команда преданных профессионалов, врачей, специалистов по
+          здравоохранению и образованию, работающих в сфере улучшения здоровья
+          матерей и новорожденных. Наша миссия - обеспечить каждую семью
+          надежной информацией и ресурсами, необходимыми для счастливой и
+          здоровой беременности и первых шагов вашего малыша.
+        </p>
+      </ContentContainer>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 100%;
+  height: 45rem;
+  background-color: #fff1ed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 2rem;
+`;
+
+const ImageContainer = styled.div`
+  width: 50%;
+  text-align: center;
+`;
+
+const ContentContainer = styled.div`
+  width: 34.125rem;
+  margin-right: 10rem;
+  h1 {
+    color: var(--neutral-100, #101010);
+    font-size: 3rem;
+    font-weight: 700;
+    line-height: 120%;
+  }
+  p {
+    color: var(--neutral-60, #757b8a);
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 160%;
+  }
+`;
